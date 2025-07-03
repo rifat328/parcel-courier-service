1171e7b225528391ec0f4a8d88a3602f87e86f88
Rifat Hossain <rifathossain328663@gmail.com>
2025-07-03 18:57:53 +0600

    feat: update parcel creation logic to use user ID from request and improve parcel assignment comments

--- controllers/parcel.controller.js
+++ controllers/parcel.controller.js
@@ -74,7 +74,10 @@ export const createParcel = async (req, res, next) => {
   const session = await mongoose.startSession();
   session.startTransaction();
   try {
-    const { customerId } = req.params;
+    // parcel.customer._id.toString();
+    const customerId = req.user._id.toString();
+    const userRole = req.user.role;
+    console.log(`Customer ID: ${customerId}`);
     const customer = await User.findById(customerId);
     if (!customer) {
       return res.status(404).json({
@@ -87,7 +90,7 @@ export const createParcel = async (req, res, next) => {
       customer: customerId,
       pickupAddress: req.body.pickupAddress || customer.address,
     };
-    const parcel = new Parcel(parcelData);
+    const parcel = await Parcel.create(parcelData);
     await parcel.save({ session });
     await session.commitTransaction();
     res.status(201).json({
@@ -104,20 +107,127 @@ export const createParcel = async (req, res, next) => {
 };
 
 export const updateParcel = async (req, res, next) => {
+  // assign [parcel to agent by admin]
+  //agent can update status of parcel
+  //customer can update -> if parcel is not assigned to agent && customer cant update the status of the parcel
+
+  const userId = req.user._id.toString();
+  const userRole = req.user.role;
+  const isAdmin = userRole === "admin";
+  const isCustomer = userRole === "customer";
+  const isAgent = userRole === "agent";
+
   const session = await mongoose.startSession();
   session.startTransaction();
+
   try {
-    const parcel = await Parcel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-      session,
-    });
+    const parcel = await Parcel.findById(req.params.id)
+      .populate("customer", "-password")
+      .populate("agent", "-password");
     if (!parcel) {
       return res.status(404).json({
         success: false,
         message: "Parcel not found",
       });
     }
+    // validation checks
+    if (isCustomer) {
+      if (parcel.customer._id.toString() !== userId) {
+        return res.status(403).json({
+          success: false,
+          message: "You can only update your own parcels",
+        });
+      }
+      // parcel already assigned to agent
+      if (parcel.agent) {
+        return res.status(403).json({
+          success: false,
+          message: "Parcel already assigned to an agent. You can't update it.",
+        });
+      }
+
+      if ("status" in req.body) {
+        return res.status(403).json({
+          success: false,
+          message: "Customers are not allowed to update status.",
+        });
+      }
+
+      const allowedFields = {
+        pickupAddress: req.body.pickupAddress || parcel.pickupAddress,
+        deliveryAddress: req.body.deliveryAddress || parcel.deliveryAddress,
+        paymentType: req.body.paymentType || parcel.paymentType,
+        codAmount: req.body.codAmount || parcel.codAmount,
+        parcelType: req.body.parcelType || parcel.parcelType,
+      };
+
+      Object.assign(parcel, allowedFields);
+      await parcel.save({ session });
+      await session.commitTransaction();
+
+      return res.status(200).json({
+        success: true,
+        message: "Parcel updated successfully by customer",
+        data: parcel,
+      });
+    }
+
+    if (isAdmin) {
+      const agentId = req.body.agent;
+      if (agentId && agentId !== parcel.agent?._id.toString()) {
+        const agent = await User.findById(agentId);
+        if (!agent || agent.role !== "agent") {
+          return res.status(404).json({
+            success: false,
+            message: "Agent not found or not authorized",
+          });
+        }
+        parcel.agent = agentId;
+      }
+      const allowedFields = {
+        pickupAddress: req.body.pickupAddress || parcel.pickupAddress,
+        deliveryAddress: req.body.deliveryAddress || parcel.deliveryAddress,
+        paymentType: req.body.paymentType || parcel.paymentType,
+        codAmount: req.body.codAmount || parcel.codAmount,
+        parcelType: req.body.parcelType || parcel.parcelType,
+        status: req.body.status || parcel.status, // Admin can update status
+      };
+      Object.assign(parcel, allowedFields);
+      await parcel.save({ session });
+
+      await session.commitTransaction();
+      return res.status(200).json({
+        success: true,
+        message: "Parcel updated successfully by admin",
+        data: parcel,
+      });
+    }
+
+    if (isAgent) {
+      if (parcel.agent?._id.toString() !== userId) {
+        return res.status(403).json({
+          success: false,
+          message: "You are not assigned to this parcel",
+        });
+      }
+
+      if ("status" in req.body) {
+        parcel.status = req.body.status;
+      }
+      if (req.body.location) {
+        parcel.location = req.body.location;
+      }
+
+      await parcel.save({ session });
+      await session.commitTransaction();
+
+      return res.status(200).json({
+        success: true,
+        message: "Parcel updated successfully by agent",
+        data: parcel,
+      });
+    }
+
     await session.commitTransaction();
     res.status(200).json({
       success: true,
