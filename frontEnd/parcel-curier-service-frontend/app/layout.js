217649244e4143e5fda19b5b2f9b57c7e6dc03f8
Rifat Hossain <rifathossain328663@gmail.com>
2025-07-07 07:55:43 +0600

    feat: initialize frontend with Next.js, React, and Tailwind CSS setup
    - Added package.json for project dependencies and scripts
    - Configured PostCSS with Tailwind CSS

---
+++ frontEnd/parcel-curier-service-frontend/app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Parcel Curier Service App",
+  description: "Parcel Curier Service App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* place layout here like navbar*/}
+        {children}
+        {/* place layout here  footer*/}
+      </body>
+    </html>
+  );
+}
