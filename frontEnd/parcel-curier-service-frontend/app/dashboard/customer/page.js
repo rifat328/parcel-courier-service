f6fe8d5156316b71ec0cdee277289bfae714398d
Rifat Hossain <rifathossain328663@gmail.com>
2026-01-05 18:55:33 +0600

    fix: correct token variable name and enhance user verification in DashboardLayout

--- frontEnd/parcel-curier-service-frontend/app/dashboard/customer/page.js
+++ frontEnd/parcel-curier-service-frontend/app/dashboard/customer/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CustomerDashboard = () => {
-  return <div className="text-2xl font-bold">CustomerDashboard</div>;
+  return <section className="text-2xl font-bold">CustomerDashboard</section>;
 };
 
 export default CustomerDashboard;
--- frontEnd/parcel-curier-service-frontend/app/dashboard/layout.js
+++ frontEnd/parcel-curier-service-frontend/app/dashboard/layout.js
@@ -6,27 +6,34 @@ import CustomerSidebar from "@/components/CustomerSidebar";
 import AgentSidebar from "@/components/AgentSidebar";
 
 export default async function DashboardLayout({ children }) {
-  const token = (await cookies()).get("token")?.value;
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
 
   //if the user is not loged in
-  if (!toekn) {
+  if (!token) {
     redirect("/sign-in");
   }
+
   const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const currentAuthenticatedUser =
+    process.env.NEXT_PUBLIC_API_GET_CURRENT_AUTHENTICATED_USER_ROUTE;
   // verify user
-  const response = await fetch(`${apiBaseUrl}/api/me`, {
+  console.log("VERIFY URL:", `${apiBaseUrl}${currentAuthenticatedUser}`);
+
+  const response = await fetch(`${apiBaseUrl}${currentAuthenticatedUser}`, {
     headers: { Authorization: `Bearer ${token}` },
     cache: "no-store",
   });
   if (!response.ok) redirect("/sign-in");
   const user = await response.json();
-
+  console.log("COOKIE STORE:", cookieStore.getAll());
   return (
     <UserProvider user={user}>
       <div className="flex min-h-screen">
         {user.role === "admin" && <AdminSidebar />}
         {user.role === "customer" && <CustomerSidebar />}
         {user.role === "agent" && <AgentSidebar />}
+        <main className="flex-1">{children}</main>
       </div>
     </UserProvider>
   );
