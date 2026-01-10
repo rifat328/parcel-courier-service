4401bbd38db481c7e75dec9c0adc1e77462f16e0
Rifat Hossain <rifathossain328663@gmail.com>
2026-01-10 22:17:18 +0600

    feat: update CustomerDashboard layout for improved grid structure and responsiveness

--- frontEnd/parcel-curier-service-frontend/app/dashboard/customer/page.js
+++ frontEnd/parcel-curier-service-frontend/app/dashboard/customer/page.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const CustomerDashboard = () => {
   return (
-    <section className="grid grid-cols-12 grid-rows-[auto_1fr_1fr] gap-2 h-full w-full font-roboto outline outline-red-500">
-      <div className="dashboard-Header col-span-12  gap-3 rounded-t-4xl bg-amber-300">
+    <section className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-12  gap-2 h-full w-full font-roboto outline outline-red-500">
+      <div className="dashboard-Header col-span-1 md:col-span-8 lg:col-span-12  gap-3 rounded-t-4xl bg-amber-300">
         <div className="flex p-5 justify-between">
           <input
             type="text"
@@ -13,16 +13,16 @@ const CustomerDashboard = () => {
           <button className="bg-black rounded-2xl p-3">Create Parcel</button>
         </div>
       </div>
-      <div className="active-parcel col-span-8 bg-card rounded-xl p-4">
+      <div className="active-parcel col-span-1 md:col-span-5 lg:col-span-8  bg-card p-4 rounded-xl ">
         <h1>Active parcel</h1>
       </div>
-      <div className="key-metrix col-span-4 space-y-4">
+      <div className="key-metrix col-span-1 md:col-span-3 lg:col-span-4  space-y-4">
         <h1>key matrix</h1>
       </div>
-      <div className="tracking-timeline col-span-12 bg-card rounded-xl p-4">
+      <div className="tracking-timeline col-span-1 md:col-span-8 lg:col-span-12 bg-card p-4 rounded-xl ">
         <h1>tracking</h1>
       </div>
-      <div className="recent-bookings col-span-12 bg-card rounded-xl p-4">
+      <div className="recent-bookings col-span-1 md:col-span-8 lg:col-span-12 bg-card p-4 rounded-xl ">
         <h1>recent booking</h1>
       </div>
     </section>
