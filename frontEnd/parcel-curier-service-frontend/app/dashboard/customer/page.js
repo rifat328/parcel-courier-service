96e8f31c7acde41a928319a6c21c4485d356f676
Rifat Hossain <rifathossain328663@gmail.com>
2026-01-17 15:28:12 +0600

    Dashboard layout fix: header => sticky, Active parcel & key matrix 100% screen , scroll snap to other screen

--- frontEnd/parcel-curier-service-frontend/app/dashboard/customer/page.js
+++ frontEnd/parcel-curier-service-frontend/app/dashboard/customer/page.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const CustomerDashboard = () => {
   return (
-    <section className="flex flex-col h-full w-full font-roboto outline outline-red-500">
-      {/* Header */}
+    <section className="flex flex-col h-screen w-full font-roboto outline outline-red-500">
+      {/*🔒 Sticky Header */}
 
-      <div className="dashboard-Header col-span-1 md:col-span-8 lg:col-span-12  gap-3 rounded-t-4xl  bg-amber-300 outline outline-red-500">
+      <header className="dashboard-Header sticky top-0 z-20 h-[80px] gap-3 rounded-t-4xl  bg-amber-300 outline outline-red-500">
         <div className="flex p-5 justify-between">
           <input
             type="text"
@@ -14,20 +14,28 @@ const CustomerDashboard = () => {
           />
           <button className="bg-black rounded-2xl p-3">Create Parcel</button>
         </div>
-      </div>
+      </header>
+
       {/* CONTENT GRID */}
-      <div className="content-grid grid grid-cols-1 md:grid-cols-8 lg:grid-cols-12 gap-4 flex-1 outline outline-red-500">
-        <div className="active-parcel  md:col-span-5 lg:col-span-8  bg-card p-4 rounded-xl outline outline-red-500">
-          <h1>Active parcel</h1>
+      {/*  Scroll Container */}
+      <div className="flex-1 overflow-y-auto mt-3  snap-mandatory snap-y scroll-smooth overscroll-contain">
+        {/* Above the folde ( full view port) */}
+        <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-12 gap-4 snap-start min-h-[calc(100vh-80px)]">
+          <div className="active-parcel  md:col-span-5 lg:col-span-8  bg-card p-4 rounded-xl outline outline-red-500">
+            <h1>Active parcel</h1>
+          </div>
+          <div className="key-metrix  md:col-span-3 lg:col-span-4  space-y-4 outline outline-red-500">
+            <h1>key matrix</h1>
+          </div>
         </div>
-        <div className="key-metrix  md:col-span-3 lg:col-span-4  space-y-4 outline outline-red-500">
-          <h1>key matrix</h1>
-        </div>
-        <div className="tracking-timeline  md:col-span-8 lg:col-span-12 bg-card p-4 rounded-xl outline outline-red-500">
-          <h1>tracking</h1>
-        </div>
-        <div className="recent-bookings  md:col-span-8 lg:col-span-12 bg-card p-4 rounded-xl outline outline-red-500">
-          <h1>recent booking</h1>
+        {/* ⬇️ Below-the-fold */}
+        <div className="grid grid-cols-1 md:grid-cols-8 lg:grid-cols-12 gap-4 snap-start">
+          <div className="tracking-timeline  md:col-span-8 lg:col-span-12 bg-card p-4 rounded-xl outline outline-red-500">
+            <h1>tracking</h1>
+          </div>
+          <div className="recent-bookings  md:col-span-8 lg:col-span-12 bg-card p-4 rounded-xl outline outline-red-500">
+            <h1>recent booking</h1>
+          </div>
         </div>
       </div>
     </section>
