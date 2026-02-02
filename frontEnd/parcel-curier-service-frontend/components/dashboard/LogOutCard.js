67c0893132878e92c2582470c78da8b30f6c7957
Rifat Hossain <rifathossain328663@gmail.com>
2026-02-02 16:59:31 +0600

    feat: Implemented LogOut card component with advanced tooltip for ux and A11y.

--- frontEnd/parcel-curier-service-frontend/components/dashboard/LogOutCard.js
+++ frontEnd/parcel-curier-service-frontend/components/dashboard/LogOutCard.js
@@ -6,8 +6,14 @@ import { useRouter } from "next/navigation";
 const LogOutCard = (props) => {
   const router = useRouter();
 
-  const logOut = () => {
-    document.cookie = "";
+  const logOut = async () => {
+    await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/${process.env.NEXT_PUBLIC_API_SIGN_OUT_ROUTE}`,
+      {
+        method: "POST",
+        credentials: "include",
+      },
+    );
     router.push("/sign-in");
   };
 
@@ -18,8 +24,10 @@ const LogOutCard = (props) => {
   return (
     <div className="bg-card-gray p-3 rounded-xl w-auto flex items-center gap-3">
       <div
-        className="log_out w-[50] h-[50] rounded-[50%] bg-accent overflow-hidden cursor-pointer"
+        className="log_out w-[50] h-[50] rounded-[50%] bg-accent overflow-hidden cursor-pointer hover:scale-110 hover:opacity-80 transition-transform duration-200 relative group"
         onClick={goToProfile}
+        aria-label="Edit Profile"
+        title="Profile"
       >
         <Image
           key={"logout_img1_women.jpg"}
@@ -29,6 +37,9 @@ const LogOutCard = (props) => {
           height={50}
           className="object-cover w-full h-full"
         />
+        <span className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-10 whitespace-nowrap">
+          Profile
+        </span>
       </div>
 
       <div className="flex flex-col">
@@ -36,7 +47,11 @@ const LogOutCard = (props) => {
         <p className="text-sm text-gray-600">{props.userRole}</p>
       </div>
 
-      <button className="ml-auto w-[40] h-[40]" onClick={logOut}>
+      <button
+        className="ml-auto w-[40] h-[40] hover:scale-110 hover:opacity-80 transition-transform duration-200 cursor-pointer relative group"
+        onClick={logOut}
+        title="Logout"
+      >
         <Image
           key={"logout_icon.png"}
           alt="logout icone"
@@ -45,6 +60,9 @@ const LogOutCard = (props) => {
           src="/logout_image/logout_icon.png"
           className="object-cover w-full h-full"
         />
+        <span className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
+          Logout
+        </span>
       </button>
     </div>
   );
