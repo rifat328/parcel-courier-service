ef25fb6636af036b9c72a10059d351fc64e9f070
Rifat Hossain <rifathossain328663@gmail.com>
2026-03-25 11:48:50 +0600

    feat:=> button gradient added on create parcel

--- frontEnd/parcel-curier-service-frontend/components/dashboard/Header.js
+++ frontEnd/parcel-curier-service-frontend/components/dashboard/Header.js
@@ -17,11 +17,13 @@ const Header = ({ isMobileOpen, setIsMobileOpen }) => {
             placeholder="Search #Id"
           />
         </div>
-        {/* all the way to right */}
-        <button className="flex justify-center items-center gap-2 bg-black hover:bg-[#1A1A1A] text-white rounded-2xl px-4 py-3 sm:px-6 text-sm font-medium transition-colors min-w-fit">
-          <Plus size={16} className="shrink-0" />
-          <span className="hidden sm:inline">Create Parcel</span>
-        </button>
+        {/* create parcel button */}
+        <div className=" p-[1.5px] rounded-2xl hover:bg-gradient-to-br from-[#4EC4D9]/80 via-[#DB9118]/80 to-[#D94E4E]/80">
+          <button className="flex justify-center items-center gap-2 bg-black hover:bg-[#1A1A1A] text-white/80 hover:text-white rounded-[14px] px-4 py-3 sm:px-6 text-sm font-medium transition-colors min-w-fit ">
+            <Plus size={16} className="shrink-0" />
+            <span className="hidden sm:inline">Create Parcel</span>
+          </button>
+        </div>
       </div>
     </header>
   );
